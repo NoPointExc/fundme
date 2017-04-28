56cef9b5eb7ac9568060adf3e38a799ed858ed1b
Jiayang, Sun <joeysun6@gmail.com>
2017-04-28 11:33:33 -0400

    add /projects
    1) add new router project
    2) add new app project

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var passport = require('./app/local-passport');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var newsfeed = require('./routes/newsfeed');
+var projects = require('./routes/projects');
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use(passport.session());
 app.use('/', index);
 app.use('/users', users);
 app.use('/newsfeed', newsfeed);
+app.use('/projects', projects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
