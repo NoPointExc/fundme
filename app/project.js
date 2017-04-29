9c4bb1c69a218d682e39988a6b3cdc359dbf638f
Jiayang, Sun <joeysun6@gmail.com>
2017-04-29 19:11:27 -0400

    GET /project/detail/?pname='Project Name' to get project detail.
    return json includes:
    detail: project info like pname, uname, description...
    likeNum: how much user like this project
    like: whether login user like this project.
    fellowNum: how much user fellow this project.
    fellow: whether login user fellowed this project.

--- app/project.js
+++ app/project.js
@@ -11,7 +11,6 @@ function get(num, after, category, keyword, done){
     if(keyword){
 	conditions.push(['description LIKE ?', '%'+keyword +'%']);
     }	
-
     sql.project.get(num, conditions, function (error, rows, fields){
 	if(error){
 	    return done(error, null);
@@ -21,4 +20,76 @@ function get(num, after, category, keyword, done){
     });
 }
 
+function detail(projectname, username, done){
+    var result = {};
+    var completed = 0;
+    log.debug('username= ' + username);
+    function onSqlDone(relation,error, rows){
+	if(error){
+	    log.debug('error');
+	    return done(error, null);
+	}else if(relation == 'detail'){
+	    result['detail'] = rows[0];
+	}else{ 
+	    log.debug(rows);
+	    result[relation+'Num'] = rows.length;
+	    result[relation] = false;
+	    if(username){
+		for(i in rows){
+		    log.debug(rows[i].uname);
+		    if(rows[i].uname == username){
+			result[relation] = true;
+			break;
+		    }
+		}
+	    }
+	}
+	completed += 1;
+	if(completed == 3){
+	    //log.debug(result);
+	    return done(null, result);
+	}
+    }
+
+    sql.project.get(1, [['pname = ?', projectname]], function(error, rows, fields){
+	return onSqlDone('detail', error, rows);
+    });
+
+    likeBy(projectname, function(error, rows){
+	return onSqlDone('like', error, rows);
+    });
+
+    fellowBy(projectname, function(error, rows){
+	return onSqlDone('fellow', error, rows);
+    });
+}
+
+function likeBy(projectname, done){
+    if(!projectname){
+	return done(null, null);
+    }
+    sql.sql.select(['uname'], 'User_project', [['pname=?', projectname], ['relation=?','like']],function (error, rows, fields){
+	if(error){
+	    return done(error, null);
+	}else{
+	    return done(null, rows);
+	}
+    }); 
+}
+
+
+function fellowBy(projectname, done){
+    if(!projectname){
+	return done(null, null);
+    }
+    sql.sql.select(['uname'], 'User_project', [['pname=?', projectname],['relation=?','fellow']],function (error, rows, fields){
+	if(error){
+	    return done(error, null);
+	}else{
+	    return done(null, rows);
+	}
+    }); 
+}
+
 module.exports.get = get;
+module.exports.detail = detail;
